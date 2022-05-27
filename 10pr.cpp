d8504a904cfb2c7e438f14a2b817927ad2473382
Namka20 <104615753+Namka20@users.noreply.github.com>
2022-05-27 13:49:09 +1100

    Update 10pr.cpp

--- 10pr.cpp
+++ 10pr.cpp
@@ -13,16 +13,16 @@ struct tnode* addtree(struct tnode* p, char* w) {
     int cond;
     if (p == NULL) {
         p = (struct tnode*)malloc(sizeof(struct tnode));
-        p->word = _strdup(w);
-        p->count = 1;
-        p->left = p->right = NULL;
+ p->word = _strdup(w);
+ p->count = 1;
+ p->left = p->right = NULL;
     }
     else if ((cond = strcmp(w, p->word)) == 0)
-        p->count++;
+ p->count++;
     else if (cond < 0)
-        p->left = addtree(p->left, w);
+ p->left = addtree(p->left, w);
     else
-        p->right = addtree(p->right, w);
+ p->right = addtree(p->right, w);
     return p;
 }
 void freemem(tnode* tree) {
