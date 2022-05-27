8000c9ba7430b96472967e9fb74de33aad275a10
Namka20 <104615753+Namka20@users.noreply.github.com>
2022-05-27 13:44:55 +1100

    Update 9pr.cpp

--- 9pr.cpp
+++ 9pr.cpp
@@ -1,46 +1,75 @@
-#include <iostream>
-using namespace std;
-const int col_razr = 3;
-int velich_razr(int chislo, int razr)
+#include "iostream"
+#include "windows.h"
+
+int max_razr(int chislo)
 {
-    while (razr > 1)
+ int max = 0;
+ while (chislo > 1)
     {
-        chislo /= 10;
-        razr--;
+ chislo /= 10;
+ max++;
     }
-    return chislo % 10;
+ return max;
 }
 
-void sort_razr(int dop_mas[10][10], int mas[10], int razr)
+int velich_razr(int chislo, int razr)
 {
-    int mas_col[10], i, j, temp = 0;
-    for (i = 0; i < 10; i++)
-        mas_col[i] = 0;
-    for (i = 0; i < 10; i++)
+ while (razr > 1)
     {
-        int a = velich_razr(mas[i], razr);
-        dop_mas[mas_col[a]][a] = mas[i];
-        mas_col[a]++;
+ chislo /= 10;
+ razr--;
     }
-    for (i = 0; i < 10; i++)
+ return chislo % 10;
+}
+
+void sort_razr(int** dop_mas, int* mas, int razr,int n)
+{
+ int* mas_col, i, j, temp = 0;
+ mas_col = new int[n];
+ for (i = 0; i < n; i++)
+ mas_col[i] = 0;
+ for (i = 0; i < n; i++)
     {
-        for (j = 0; j < mas_col[i]; j++)
+ int a = velich_razr(mas[i], razr);
+ dop_mas[mas_col[a]][a] = mas[i];
+ mas_col[a]++;
+    }
+ for (i = 0; i < n; i++)
+    {
+ for (j = 0; j < mas_col[i]; j++)
         {
-            mas[temp] = dop_mas[j][i];
-            temp++;
+ mas[temp] = dop_mas[j][i];
+ temp++;
         }
     }
 }
 
 int main()
 {
-    setlocale(LC_ALL, "rus");
-    int razr, i;
-    int mas[10] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-    int dop_mas[10][10];
-    for (razr = 1; razr < 4; razr++)
-        sort_razr(dop_mas, mas, razr);
-    for (i = 0; i < 10; i++)
-        cout << mas[i] << endl;
-    return 0;
+     setlocale(0, "rus");
+ using namespace std;
+ int n, col_razr = 0;
+ SetConsoleCP(1251);
+ SetConsoleOutputCP(1251);
+ int razr, i, * mas, ** dop_mas;
+ cout << "Введите размер последовательности: ";
+ cin >> n;
+ mas = new int[n];
+ cout << "Введите элементы последовательности: " << endl;
+ for (int i = 0; i < n; i++) //Рандом заполнение массива
+    {
+ mas[i] = rand() % 100; // заполняем массив случайными числами
+ cout << "[" << i + 1 << "]="<<mas[i]<<endl;
+    }
+ dop_mas = new int* [n];
+ for (i = 0; i < n; i++)
+ dop_mas[i] = new int[n];
+ for (i = 0; i < n; i++)
+ if (col_razr < max_razr(mas[i]))
+ col_razr = max_razr(mas[i]);
+ for (razr = 1; razr <= col_razr; razr++)
+ sort_razr(dop_mas, mas, razr,n);
+ for (i = 0; i < n; i++)
+ cout << mas[i] << endl;
+ return 0;
 }
